c02efbe83122a364b5cd26b845b0321759dfd5cf
Parazitol-EU <74571658+Parazitol-EU@users.noreply.github.com>
2020-11-18 02:13:57 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,255 @@
+var new_lang = {
+
+    "lt1": "SNAŽAN UDARAC PARAZITIMA",
+
+"lt2": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Normalizira rad gastrointestinalnog trakta",
+
+"lt3": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Uništava sve vrste glista",
+
+"lt4": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Odstranjuje otpadne proizvode parazita",
+
+"lt5": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Sprječava ponovnu infekciju",
+
+"lt6": "POSEBNA<br> PONUDA",
+
+"lt7": "do kraja akcije ostalo je",
+
+"lt8": "<div class=\"price_old\"></div>  Obična cijena",
+
+"lt9": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: visible; animation-delay: 2s; animation-iteration-count: 3; animation-name: pulse;\"></div>  Cijena tijekom akcije",
+
+"lt10": "Naručiti",
+
+"lt11": "PREMA NAJNOVIJIM PODACIMA WHO-a<br>, VIŠE OD 1 MILIJARDE LJUDI ZARAŽENO JE PARAZITIMA",
+
+"lt12": "90% njih uopće ne shvaća da su postali životna sredina za ishranu glista",
+
+"lt13": "JESTE LI SIGURNI DA NISTE UKLJUČENI U NJIHOV BROJ? PROVJERITE SE ZA ZNAKOVE INFEKCIJE!",
+
+"lt14": "ZNAKOVI HELMINTOZE",
+
+"lt15": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Osjećaj umora",
+
+"lt16": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Glavobolja i tegobe",
+
+"lt17": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Upala gornjih dišnih putova",
+
+"lt18": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Razdražljivost i agresija",
+
+"lt19": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Različite vrste osipa na koži",
+
+"lt20": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Gubitak tjelesne težine, čak i uz dobar apetit",
+
+"lt21": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Problemi sa spavanjem",
+
+"lt22": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Alergije (suzne oči, osip, curenje iz nosa)",
+
+"lt23": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Česte prehlade, angina, začepljen nos",
+
+"lt24": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Bolovi u zglobovima i mišićima",
+
+"lt25": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Povećani limfni čvorovi",
+
+"lt26": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Poremećaj probave: osjećaj nadutosti, zatvor ili proljev",
+
+"lt27": "NAČINI ZARAŽIVANJA HELMINTIMA",
+
+"lt28": "Loše oprano povrće, voće, jagode",
+
+"lt29": "Nedovoljno isprženo meso i riba",
+
+"lt30": "Kontaminirana voda ili tlo",
+
+"lt31": "Kontakt sa kućnim ljubimcima",
+
+"lt32": "Javna mjesta i prijevoz",
+
+"lt33": "Ubodi insekata",
+
+"lt34": "Restorani i fast food restorani",
+
+"lt35": "ZAPOČNITE LIJEČENJE ODMAH!",
+
+"lt36": "POČETI",
+
+"lt37": "Paraziti <br>koji uzrokuju strašne komplikacije:",
+
+"lt38": "CISTICERK",
+
+"lt39": "Najčešće (više od 60%) cisticerk ulazi u mozak, rjeđe u skeletne mišiće i oči. Živi od 5 do 30 godina, uzrokuje <span>kancerogene tumore</span> ",
+
+"lt40": "GLISTE",
+
+"lt41": "Oni se nastanjuju i razmnožavaju u organima probave. Dovode do stvaranja <span>čira</span>  i <span>opsežnog krvarenja</span> ",
+
+"lt42": "ANCYLOSTOMA",
+
+"lt43": "Oštećuju sluznicu i hrane se krvlju koja se pri tome oslobađa. Ostavlja rane koje krvare, što dovodi do razvoja <span>teške anemije</span>  i <span>sepse</span> ",
+
+"lt44": "LISIČJA TRAKAVICA",
+
+"lt45": "Dolazi do odumiranja tkiva zahvaćenog organa. Osim toga, krvne žile se razgrađuju i <span>prestaju funkcionirati</span> ",
+
+"lt46": "VIŠEKRATNO DJELOVANJE <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt47": "UNIŠTAVA",
+
+"lt48": "sve moguće vrste infestacija u najkraćem mogućem roku",
+
+"lt49": "UKLANJA",
+
+"lt50": "otpadne proizvode parazita iz organizma",
+
+"lt51": "POTIČE",
+
+"lt52": "čišćenje organizma od toksina",
+
+"lt53": "OBNAVLJA",
+
+"lt54": "rad imunološkog, endokrinog, probavnog, živčanog, kardiovaskularnog sustava",
+
+"lt55": "ŠTITI",
+
+"lt56": "od ponovne infekcije, formira patogeno okruženje u ljudskom organizmu i pojačava imunitet",
+
+"lt57": "NARUČITI",
+
+"lt58": "SNAŽAN PRIRODNI SASTAV <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt59": "MORINGA",
+
+"lt60": "Zbog svog jedinstvenog sastava čisti organizam od štetnih tvari i teških metala. Učinkovito se bori protiv gastrointestinalnih parazita, uništavajući tuđe DNK. Pročišćava crijeva i žučni trakt.",
+
+"lt61": "TAHEEBO (TABEBUIA AVELLANADAE)",
+
+"lt62": "Lapahol - posebno jedinjenje koje se nalazi u kori - onemogućava razmnožavanje patogenih mikroorganizama i usporava razvoj unutarstaničnih parazita.",
+
+"lt63": "CRNI ORAH",
+
+"lt64": "Elementi crnog oraha remete metabolizam crva, uzrokuju paralizu mišićnog tkiva parazita, također povećavaju imunitet i zaštitne funkcije organizma, pročišćavaju krv od toksina.",
+
+"lt65": "LISTOVI MASLINE",
+
+"lt66": "Sadrže oleuropein, koji ima antiparazitsko i protugljivično djelovanje, smanjuje oksidacijsko oštećenje u krvi, u jetri, bubrezima i tkivu mozga.",
+
+"lt67": "KORIJEN KURKUME",
+
+"lt68": "Korijen kurkume sadrži cineol, kurkumin i vitamin P. Cineol normalizira rad probavnih organa i inhibira vitalnu aktivnost glista, ameron ima antiparazitski učinak protiv oblića (nematoda), vitamin P obnavlja rad organa koji stvaraju krv i pročišćava krv.",
+
+"lt69": "ASAFETIDA",
+
+"lt70": "Poznato po snažnim baktericidnim i antimikrobnim svojstvima. Posebno je cijenjena njegova sposobnost uklanjanja trakavica-parazita iz gastrointestinalnog trakta.",
+
+"lt71": "ZAŠTO JE <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span>BOLJI OD DRUGIH PREPARATA?",
+
+"lt72": "KEMIJSKI PREPARATI",
+
+"lt74": "KEMIJSKI PREPARATI",
+
+"lt75": "Štetne kemikalije<br> u sastavu <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt76": "Snažan udar na unutarnje <br>organe i sustave <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt77": "Doprinose <br>smanjenju imuniteta <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt78": "Nanose štetu organima gastrointestinalnog trakta: <br> <span>jetre, želuca, gušterače, crijeva, izazivaju <br>gastritis i ulkus.</span> ",
+
+"lt80": "Potpuno <br>prirodni sastav <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt81": "Nema <br>nuspojava, štiti <br>unutarnje organe <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt82": "Povećava imunitet<br> i otpornost <br>organizma na infekcije <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt83": "Povoljno djeluje na gastrointestinalni trakt:<br> <span> poboljšava odljev žuči, potiče rad crijeva</span> ",
+
+"lt84": " <span>Parazitol</span> BLAGO ZA ČOVJEKA I UBOJICA ZA HELMINTE",
+
+"lt85": "»Većina ljudi uopće ne sumnja da je organizam svakog drugog čovjeka ZARAŽEN PARAZITIMA.",
+
+"lt86": "U 95% slučajeva prisutnost helminta ne uzrokuje nikakve simptome, sve dok ne dođe do nepopravljive štete unutarnjim organima i ne naruši se rad svih sustava organizma.",
+
+"lt87": "Na današnji dan samo je preparat Parazitol sposoban da u POTPUNOSTI očisti organizam od bilo kojih parazita, kojih postoji više od 250 vrsta. On stvara takvo okruženje u ljudskom organizmu koje uništava proces parazitiranja.",
+
+"lt88": "Iskustvo mojih pacijenata potvrđuje da je Parazitol najučinkovitije i najsigurnije sredstvo za uništavanje i uklanjanje SVIH vrste infestacija, uključujući njihova jajašca, što ne dopušta ponovnu infekciju.",
+
+"lt89": "Pozitivan učinak postiže se u 100% slučajeva«.",
+
+"lt90": "Mate Granić, ljekar parazitolog",
+
+"lt91": "DODATNO DJELOVANJE <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt92": "Pomaže u čišćenju organa i tkiva, neutralizira intoksikaciju kao posljedicu parazitske infekcije",
+
+"lt93": "Obnavlja sluznicu, pročišćava krv, limfu, međustanične tekućine",
+
+"lt94": "Normalizira redoks ravnotežu i tako sprječava stvaranje stanica raka",
+
+"lt95": "Normalizira redoks ravnotežu i tako sprječava stvaranje stanica raka",
+
+"lt96": "Pomaže u uklanjanju ulkusa i gastritisa uništavanjem bakterija koje ih izazivaju",
+
+"lt97": "Obnavlja i normalizira funkcije napadnutih organa",
+
+"lt98": "Blagotvorno djeluje na organe gastrointestinalnog trakta",
+
+"lt99": "Poboljšava stanje kože, kose i noktiju",
+
+"lt100": "NARUČITI",
+
+"lt101": "RECENZIJE KUPACA",
+
+"lt102": "<div class=\"slide-description-title\">Milena, Split</div>  »Jako mi se svidjelo ovo sredstvo. Moj suprug i ja smo ga uzimali. Oboje smo imali različite manifestacije: hladan znoj, bila je tekuća stolica, pojavilo se malo akni i strašno nas je boljela glava. Već nakon 1,5 tjedna pogoršanje je prošlo i sada se oboje osjećamo odlično! Parazitol radi vrlo blago, ali učinkovito i pouzdano«.",
+
+"lt103": "<div class=\"slide-description-title\">Suzana, Rijeka</div>  »Osjećala sam se loše nekoliko tjedana, uradila sam analize koje su pokazale da su u pitanju paraziti. Uzimala sam tablete prema liječničkom receptu, ali svaki dan mi je bilo samo gore. Ne znam što bi mi se dogodilo da nisam saznala za novi preparat Parazitol. On me je spasio. Nakon mjesec dana ponovila sam analize i sve je bilo uredno. Sad ću preparat Parazitol uzimati jednom godišnje za prevenciju«.",
+
+"lt104": "<div class=\"slide-description-title\">Ante, Zagreb</div> »Naš mačak ne sjedi kod kuće, voli hodati po dvorištu. Tako nam je donio gliste. Kod mene je sve bilo u redu, ali jadna supruga se mučila. U početku je noću škripala zubima, a potom je dobila i temperaturu, i povraćala je. Zajedno smo se liječili jer me je, vjerojatno, mogla zaraziti. Uzimali smo Parazitol i, na kraju, sve je prošlo«.",
+
+"lt105": "<div class=\"slide-description-title\">Milena, Split</div>  »Jako mi se svidjelo ovo sredstvo. Moj suprug i ja smo ga uzimali. Oboje smo imali različite manifestacije: hladan znoj, bila je tekuća stolica, pojavilo se malo akni i strašno je boljela glava. Već nakon 1,5 tjedna pogoršanje je prošlo i sada se oboje osjećamo odlično! Parazitol radi vrlo blago, ali učinkovito i pouzdano«.",
+
+"lt106": "<div class=\"slide-description-title\">Suzana, Rijeka</div>  »Osjećala sam se loše nekoliko tjedana, uradila sam analize koje su pokazale da su u pitanju paraziti. Uzimala sam tablete prema liječničkom receptu, ali svaki dan mi je bilo samo gore. Ne znam što bi mi se dogodilo da nisam saznala za novi preparat Parazitol. On me je spasio. Nakon mjesec dana ponovila sam analize i sve je bilo uredno. Sad ću preparat Parazitol uzimati jednom godišnje za prevenciju«.",
+
+"lt107": "<div class=\"slide-description-title\">Ante, Zagreb</div>  »Naš mačak ne sjedi kod kuće, voli hodati po dvorištu. Tako nam je donio gliste. Kod mene je sve bilo u redu, ali jadna supruga se mučila. U početku je noću škripala zubima, a potom je dobila i temperaturu, i povraćala je. Zajedno smo se liječili jer me je, vjerojatno, mogla zaraziti. Uzimali smo Parazitol i, na kraju, sve je prošlo«.",
+
+"lt108": "<div class=\"slide-description-title\">Milena, Split</div>  »Jako mi se svidjelo ovo sredstvo. Moj suprug i ja smo ga uzimali. Oboje smo imali različite manifestacije: hladan znoj, bila je tekuća stolica, pojavilo se malo akni i strašno nas je boljela glava. Već nakon 1,5 tjedna pogoršanje je prošlo i sada se oboje osjećamo odlično! Parazitol radi vrlo blago, ali učinkovito i pouzdano«.",
+
+"lt110": "SNAŽAN UDARAC PARAZITIMA",
+
+"lt111": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Normalizira rad gastrointestinalnog trakta",
+
+"lt112": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Uništava sve vrste glista",
+
+"lt113": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Odstranjuje otpadne proizvode parazita",
+
+"lt114": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Sprječava ponovnu infekciju",
+
+"lt115": "POSEBNA<br> PONUDA",
+
+"lt116": "do kraja akcije ostalo je",
+
+"lt117": "<div class=\"price_old\"></div>  Obična cijena",
+
+"lt118": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: hidden; animation-delay: 2s; animation-iteration-count: 3; animation-name: none;\"></div>  Cijena tijekom akcije",
+
+"lt119": "Naručiti",
+
+"lt120": "Kako mi radimo",
+
+"lt121": "Ostavite zahtjev na našoj web stranici",
+
+"lt122": "Operater će Vas kontaktirati radi potvrde narudžbe",
+
+"lt123": "Plaćanje pouzećem – roba se plaća po dostavi narudžbe"
+
+    
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
